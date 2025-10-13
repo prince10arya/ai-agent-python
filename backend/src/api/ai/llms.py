8d2ae156908842f8626cf938195c8d1adc0ac8a7
Prince Kumar <kumarprince729781@gmail.com>
2025-10-13 15:29:20 +0530

    change in the llm model

--- backend/src/api/ai/llms.py
+++ backend/src/api/ai/llms.py
@@ -1,19 +1,15 @@
 import os
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import init_chat_model
 
-OPENAI_BASE_URL = os.environ.get("OPENAI_BASE_URL")
-OPENAI_MODEL_NAME = os.environ.get("OPENAI_MODEL_NAME")
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
-if not OPENAI_API_KEY:
-    raise NotImplementedError("`OPENAI_API_KEY` is required")
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+
+if not GROQ_API_KEY:
+    raise NotImplementedError("`GROQ_API_KEY` is required")
 
 
 
 
 def get_openai_llm():
-    openai_params = {"model": OPENAI_MODEL_NAME, "api_key": OPENAI_API_KEY}
-
-    if OPENAI_BASE_URL:
-        openai_params["base_url"] = OPENAI_BASE_URL
-    return ChatOpenAI(**openai_params)
+    model = init_chat_model("llama-3.1-8b-instant", model_provider="groq")
+    return model
